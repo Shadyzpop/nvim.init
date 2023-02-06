edc1e6f0d0cff7943fb760dc128b43b143ddf84d
shadyzpop <imedevil@yahoo.com>
2023-02-06 03:47:08 +0300

    update

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -3,3 +3,10 @@ require('ufo').setup({
         return {'treesitter', 'indent'}
     end
 })
+
+-- vim.keymap.set('n', 'K', function()
+--     local winid = require('ufo').peekFoldedLinesUnderCursor()
+--     if not winid then
+--         vim.lsp.buf.hover()
+--     end
+-- end, {noremap = false, silent = true})
