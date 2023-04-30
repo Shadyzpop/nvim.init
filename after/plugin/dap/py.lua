1b7ef51a37b1090ad535a4df554a0717e9963b2d
shadyzpop <imedevil@yahoo.com>
2023-04-30 12:10:57 +0300

    add(feat): new py dap, highlighter and autocmd hooks

---
+++ after/plugin/dap/py.lua
@@ -0,0 +1,87 @@
+local dap = require('dap')
+
+-- adapter
+dap.adapters.python = function(cb, config)
+  if config.request == 'attach' then
+    ---@diagnostic disable-next-line: undefined-field
+    local port = (config.connect or config).port
+    ---@diagnostic disable-next-line: undefined-field
+    local host = (config.connect or config).host or '127.0.0.1'
+    cb({
+      type = 'server',
+      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+      host = host,
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  else
+    cb({
+      type = 'executable',
+      command = '/usr/local/bin/python3',
+      args = { '-m', 'debugpy.adapter' },
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  end
+end
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch',
+    name = "Launch file",
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}", -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/local/bin/python3'
+      end
+    end,
+  },
+  {
+    type = 'python',
+    request = 'launch',
+    name = "Launch with args",
+
+    program = "${file}",
+    pythonPath = function()
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/local/bin/python3'
+      end
+    end,
+    args = function()
+      -- return type is {'str'}
+      local args = vim.fn.input('args: ')
+      if args == '' then
+        return nil
+      end
+
+      -- respect quoted args
+      local ret = vim.split(args, ',', true)
+      -- trim
+      ret = vim.tbl_map(function(arg)
+        return vim.trim(arg)
+      end, ret)
+
+      return ret
+    end,
+  }
+}
---
+++ lua/mango/autocmd.lua
@@ -0,0 +1,53 @@
+-- autocmd
+-- vim.cmd([[autocmd TextChanged,TextChangedI <buffer> silent write]])
+
+-- auto format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("BufAutoFormat", { clear = true }),
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+    vim.lsp.buf.format()
+  end
+})
+
+-- exit terminal when quitting nvim
+-- vim.api.nvim_create_autocmd("TermClose", {
+--   group = vim.api.nvim_create_augroup("TermClose", { clear = true }),
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("silent! !kill -9 $(ps -o ppid= -p $PPID)")
+--   end
+-- })
+
+vim.api.nvim_create_augroup('bufcheck', { clear = true })
+
+-- return to the last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group    = 'bufcheck',
+  pattern  = '*',
+  callback = function()
+    -- if fn.line("'\"") > 0 and fn.line("'\"") <= fn.line("$") then
+    --   fn.setpos('.', fn.getpos("'\""))
+    --   -- vim.cmd('normal zz') -- how do I center the buffer in a sane way??
+    --   vim.cmd('silent! foldopen')
+    -- end
+  end
+})
+
+-- save the last edit position when leaving buffers
+vim.api.nvim_create_autocmd('FocusLost', {
+  group    = 'bufcheck',
+  pattern  = '*',
+  callback = function()
+    vim.cmd('silent! wa')
+  end
+})
+
+-- on vim leave, save all files
+vim.api.nvim_create_autocmd('VimLeave', {
+  group    = 'bufcheck',
+  pattern  = '*',
+  callback = function()
+    vim.cmd('silent! wa')
+  end
+})
